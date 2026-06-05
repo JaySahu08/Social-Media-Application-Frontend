b1ae164618afef36548e197682c1e13eb35217ca
jay sahu <jaysahu0805@email.com>
2026-06-05 16:40:21 +0530

    adding stuffs

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
-const API_BASE_URL = 'http://localhost:8080/api'
+import { API_BASE_URL } from '../config';
 
 async function request(path, options = {}) {
-    const response = await fetch(`${API_BASE_URL}${path}`, {
+    const response = await fetch(`${API_BASE_URL}/api${path}`, {
         headers: {
             'Content-Type': 'application/json',
             ...options.headers,
